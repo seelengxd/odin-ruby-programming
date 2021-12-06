88f8e64ab9dd94a5cabce637b691eb35cb268ca0
seeleng <seelengxd@gmail.com>
2021-12-06 17:13:29 +0800

    fixed bug with each_with_index and testcases

--- advanced/custom-enumerables.rb
+++ advanced/custom-enumerables.rb
@@ -13,8 +13,10 @@ module Enumerable
 
   def my_each_with_index
     if block_given?
-      for i in 0...self.length do 
-        yield(self[i], i)
+      index = 0
+      my_each do |item| 
+        yield(item, index)
+        index += 1
       end
     else
       Enumerator.new do |yielder|
@@ -117,15 +119,15 @@ p arr.map.take 3
 # my_inject test
 
 # Sum some numbers
-p (5..10).reduce(:+)                             #=> 45
+p (5..10).my_inject(:+)                             #=> 45
 # Same using a block and inject
-p (5..10).inject { |sum, n| sum + n }            #=> 45
+p (5..10).my_inject { |sum, n| sum + n }            #=> 45
 # Multiply some numbers
-p (5..10).reduce(1, :*)                          #=> 151200
+p (5..10).my_inject(1, :*)                          #=> 151200
 # Same using a block
-p (5..10).inject(1) { |product, n| product * n } #=> 151200
+p (5..10).my_inject(1) { |product, n| product * n } #=> 151200
 # find the longest word
-longest = %w{ cat sheep bear }.inject do |memo, word|
+longest = %w{ cat sheep bear }.my_inject do |memo, word|
   memo.length > word.length ? memo : word
 end
 p longest                                        #=> "sheep"
